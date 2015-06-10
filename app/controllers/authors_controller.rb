d3028087975ee2410c51800a3201fa541cfdac84
carlos99 <carlos.cw2106@gmail.com>
2015-06-10 14:11:52 -0600

    In the authors_controller the index, new, create and show methods has been created. In the authors_spec the testfor the method before mentioned has been created. The flash message has been added. In order to start with a little styles, the bootstra-sass and bootstra-form gems has been added. In the views. The form partial for a new author has been created. The new view has been added to.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,4 +8,24 @@ class AuthorsController < ApplicationController
 		@author = Author.find(params[:id])
 		#binding.pry
 	end
+
+	def new
+		@author = Author.new
+	end
+
+	def create
+		@author = Author.new(author_params)
+		if @author.save
+			flash[:success] = 'Author has been created successfully'
+			redirect_to @author
+		else
+			flash[:danger] = 'Author has not been created'
+			render :new
+		end	
+	end
+
+	private
+		def author_params
+			params.require(:author).permit(:first_name, :last_name)
+		end
 end
--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -27,9 +27,56 @@ describe AuthorsController do
 
 		it "Renders the show template" do
 			#author = Fabricate(:author)
-
 			get :show, id: author
 			expect(response).to render_template :show
 		end
 	end
+
+	describe "GET #new" do
+		it "Assigns a new  author object to the @author variable" do
+			get :new
+			expect(assigns(:author)).to be_instance_of(Author)
+		end
+
+		it "Renders the new template" do
+			get :new
+			expect(response).to render_template('new')
+		end
+	end
+
+	describe "POST #create" do
+		context "A successful create" do
+			it "saves the new author object" do
+			post :create, author: Fabricate.attributes_for(:author)
+			expect(Author.count).to eq(1)
+		end
+
+		it "redict to the show action" do
+			post :create, author: Fabricate.attributes_for(:author)
+			expect(response).to redirect_to author_path(Author.first)
+		end
+
+		it "sets the success flash message" do
+			post :create, author: Fabricate.attributes_for(:author)
+			expect(flash[:success]).to eq('Author has been created successfully')
+		end
+
+		context "An Unsuccessful create" do
+			it "Does not save the new author object with invalid inputs" do
+				post :create, author: Fabricate.attributes_for(:author, first_name: nil)
+				expect(Author.count).to eq(0)
+			end
+
+			it "Renders the new template" do
+				post :create, author: Fabricate.attributes_for(:author, first_name: nil)
+				expect(response).to render_template :new
+			end
+
+			it "sets the failure flash message" do
+			post :create, author: Fabricate.attributes_for(:author, first_name: nil)
+			expect(flash[:danger]).to eq('Author has not been created')
+		end
+		end
+	end
+end
 end
