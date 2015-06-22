3ebbe43ba067f80207529baf42193d36bdccd429
carlos99 <carlos.cw2106@gmail.com>
2015-06-21 21:20:17 -0600

    Adding book spec, migration adn model.

---
+++ db/migrate/20150622023336_create_books.rb
@@ -0,0 +1,16 @@
+class CreateBooks < ActiveRecord::Migration
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :isbn, limit: 13
+      t.text :description
+      t.date :published_at
+      t.references :publisher, index: true
+      t.intger :page_count
+      t.decimal :price, precision: 16, scale: 2
+
+      t.timestamps null: false
+    end
+    add_foreign_key :books, :publishers
+  end
+end
